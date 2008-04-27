a4881afbbcb741dde075cefadda5a0fc0e8f95db
wolfgang.engel <wolfgang.engel@7fa7efda-f44a-0410-a86a-c1fddb4bcab7>
2008-04-27 02:20:00 +0000

    git-svn-id: http://oolongengine.googlecode.com/svn@24 7fa7efda-f44a-0410-a86a-c1fddb4bcab7

---
+++ trunk/Oolong Engine2/Include/Physics_retired.h
@@ -0,0 +1,13 @@
+#ifndef PHYSICS_H_
+#define PHYSICS_H_
+
+#include "../Physics/CollisionDetector/AABB.h"
+#include "../Physics/CollisionDetector/Sphere.h"
+#include "../Physics/CollisionDetector/Plane.h"
+#include "../Physics/CollisionDetector/AABB_inline.h"
+#include "../Physics/CollisionDetector/Sphere_inline.h"
+#include "../Physics/CollisionDetector/Plane_inline.h"
+#include "../Physics/CollisionDetector/Frustum.h"
+#include "../Physics/CollisionDetector/Frustum_inline.h"
+
+#endif

9dceebbb1ae995180a1994449d6ad7d35b50adc7
wolfgang.engel <wolfgang.engel@7fa7efda-f44a-0410-a86a-c1fddb4bcab7>
2008-12-07 17:53:42 +0000

    That was genius ... competiting C++ memory function definitions after the new memory manager. Moved it to the right place.
    git-svn-id: http://oolongengine.googlecode.com/svn@110 7fa7efda-f44a-0410-a86a-c1fddb4bcab7

--- trunk/Oolong Engine2/Include/MemoryManager.h
+++ trunk/Oolong Engine2/Include/MemoryManager.h
@@ -4,13 +4,15 @@
 #ifdef __APPLE__
 #include <TargetConditionals.h>
 
+#include "../Utility/MemoryManager/MemoryTemplates.h"
+
 #if((TARGET_IPHONE_SIMULATOR == 0) && (TARGET_OS_IPHONE == 1) && (!_NOMEMORYMANAGER))
 #include "../Utility/MemoryManager/mmgr.h"
 #else
 #include "../Utility/MemoryManager/nommgr.h"
 #endif
 
-#include "../Utility/MemoryManager/MemoryTemplates.h"
+
 
 #endif
 #endif

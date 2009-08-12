752843affa74343be92720a412adafc95881cd5d
mozeal <mozeal@7fa7efda-f44a-0410-a86a-c1fddb4bcab7>
2009-08-12 14:47:33 +0000

    Example to play Video Title before start your game. This is just a quick demo. Need to fine tune more.
    - Do not rotate screen - if you do so, OS will rotate using UIKit which will drop our FPS 20% - keep it portrait. Oolong rotate by Projection matrix rotate
    - Oolong use Landscape Right but Media player use default as Landscape Left so, you have to edit your video upside down. (I will add code to rotate oolong to another way soon when have free time :)
    - Use Default.png same as your first frame to smooth transition to start video.
    
    -- Jimmy --
    
    git-svn-id: http://oolongengine.googlecode.com/svn@163 7fa7efda-f44a-0410-a86a-c1fddb4bcab7

---
+++ trunk/Oolong Engine2/Examples/Renderer/Tutorials/01 SkeletonXIB VideoTitle/Classes/Delegate.h
@@ -0,0 +1,23 @@
+//
+//  SkeletonXIBAppDelegate.h
+//  SkeletonXIB
+//
+//  Created by Jim on 7/25/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MediaPlayer/MediaPlayer.h>
+#import "GraphicsDevice.h"
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+	EAGLView2 *glView;
+	MPMoviePlayerController* theMovie;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet EAGLView2 *glView;
+
+@end
+
---
+++ trunk/Oolong Engine2/Examples/Renderer/Tutorials/01 SkeletonXIB VideoTitle/Classes/Delegate.mm
@@ -0,0 +1,78 @@
+//
+//  SkeletonXIBAppDelegate.m
+//  SkeletonXIB
+//
+//  Created by Jim on 7/25/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import "Delegate.h"
+#import "App.h"
+
+@implementation AppDelegate
+
+@synthesize window;
+@synthesize glView;
+
+//CONSTANTS:
+#define kFPS			60.0
+#define kSpeed			10.0
+
+static CShell *shell = NULL;
+
+- (void) update
+{
+	if(!shell->UpdateScene())
+		printf("UpdateScene error\n");
+	
+    if(!shell->RenderScene())
+		printf("RenderScene error\n");
+	
+	[glView swapBuffers];
+}
+
+-(void)myMovieFinishedCallback:(NSNotification*)aNotification
+{
+    MPMoviePlayerController* _theMovie=[aNotification object];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:MPMoviePlayerPlaybackDidFinishNotification object:_theMovie];
+    [_theMovie release];
+	
+
+    [window makeKeyAndVisible];
+
+	// create our rendering timer
+	[NSTimer scheduledTimerWithTimeInterval:(1.0 / kFPS) target:self selector:@selector(update) userInfo:nil repeats:YES];	
+}
+
+- (void)applicationDidFinishLaunching:(UIApplication *)application { 
+	// Override point for customization after application launch
+	
+	if(!shell->InitApplication())
+		printf("InitApplication error\n");
+	
+	NSString *name = @"Title";
+	NSString *path = [[NSBundle mainBundle] pathForResource:name ofType:@"m4v"];
+	theMovie=[[[MPMoviePlayerController alloc] initWithContentURL:[NSURL fileURLWithPath:path]] retain];
+	[[NSNotificationCenter defaultCenter] addObserver:self
+											 selector:@selector(myMovieFinishedCallback:)
+												 name:MPMoviePlayerPlaybackDidFinishNotification
+											   object:theMovie];
+	[theMovie play];
+	[name release];
+	
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+	if(!shell->QuitApplication())
+		printf("QuitApplication error\n");	
+	if( glView )
+		[glView releaseContext];
+}
+
+- (void)dealloc {
+    //[window release];
+    [super dealloc];
+}
+
+
+@end
